1932f479ec13b62ade0116969fde06522bac1973
Wastend <wastend318@gmail.com>
2024-11-02 12:18:10 +0600

    edit auth page

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,16 @@ import { useState } from 'react';
 import AuthPage from './pages/auth/AuthPage';
 import PageWrapper from "./components/PageWrapper/PageWrapper";
 import appRoutes from "./routes/appRoutes";
-import RegisterPage from './pages/register/RegisterPage';
 
 function App() {
 
-  const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'))
-  // const [isAuth, setIsAuth] = useState(10)
+  const [isAuth, setIsAuth] = useState(false)
 
   return (
     <BrowserRouter>
       {
-        isAuth === null ?
+        isAuth === false ?
           <Routes>
-            <Route
-              path='/register'
-              element={<RegisterPage setIsAuth={setIsAuth} />}
-            />
             <Route
               path='*'
               element={<AuthPage setIsAuth={setIsAuth} />}
--- client/src/pages/auth/AuthPage.jsx
+++ client/src/pages/auth/AuthPage.jsx
@@ -5,27 +5,21 @@ import './AuthPage.css'
 
 export default function AuthPage(props) {
 
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
 
-    async function submitHandle(e) {
-        e.preventDefault()
-        const res = await authHandle(name, password)
-        if (res) {
-            localStorage.setItem('isAuth', true)
-            props.setIsAuth(true)
-        }
-    }
+  async function submitHandle(e) {
+    e.preventDefault()
+    props.setIsAuth(true)
+  }
 
-    return (
-        <div className='auth-page'>
-            <form onSubmit={submitHandle}>
-                <h3>Authorization</h3>
-                <input value={name} type="text" minLength="5" maxLength={255} onChange={(e) => setName(e.target.value)} />
-                <input value={password} type="password" minLength="4" maxLength={255} onChange={(e) => setPassword(e.target.value)} />
-                <button id='auth-button' type='submit'>Enter</button>
-                <p>Or you can <Link to='/register'>register</Link></p>
-            </form>
-        </div>
-    )
+  return (
+    <div className='auth-page'>
+      <form onSubmit={submitHandle}>
+        <h3>Authorization</h3>
+        <input value={name} type="text" placeholder='Name' minLength="1" maxLength={255} onChange={(e) => setName(e.target.value)} />
+        <button id='auth-button' type='submit'>Enter</button>
+      </form>
+    </div>
+  )
 }
